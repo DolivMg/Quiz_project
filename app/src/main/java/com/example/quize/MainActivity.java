2900b6e00e646dcaecfc929807835cef9e393d7f
Vasa <doliv1092000@gmail.com>
2019-12-05 13:43:38 +0200

    wrote logic mainActivity

--- app/src/main/java/com/example/quize/MainActivity.java
+++ app/src/main/java/com/example/quize/MainActivity.java
@@ -2,17 +2,57 @@ package com.example.quize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private final int ID = 2131230889;
+    private int topic;
+    private Button buttonGoToQuiz;
+    private Button buttonGoToStatistic;
+    private RadioGroup radioGroupChooseTopic ;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        }
 
+        buttonGoToQuiz = (Button) findViewById(R.id.buttonGoToGame);
+        buttonGoToStatistic = (Button) findViewById(R.id.buttonGoToStatistic);
+        radioGroupChooseTopic = (RadioGroup) findViewById(R.id.chooseTopicGroup);
+
+        /***/
+        buttonGoToQuiz.setOnClickListener(this);
+        buttonGoToStatistic.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonGoToGame:
+                topic= radioGroupChooseTopic.getCheckedRadioButtonId()-ID;
+
+                Intent intent = new Intent(this, QuizActivity.class);
+                intent.putExtra("Topic", topic);
+                startActivity(intent);
+
+                break;
+            case R.id.buttonGoToStatistic:
+
+                break;
+        }
+    }
 }
